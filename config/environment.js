24d4816162c4982056ef7d39cf421d00ca8947c9
amirullah23 <amirullah.kasang@gmail.com>
2019-11-29 16:38:05 +0700

    fix port again

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 
 module.exports = {
-    PORT: 5000 || process.env.PORT, 
+    PORT: process.env.PORT || 5000, 
     DATABASE: process.env.DATABASE ,
     DATABASE_NAME: process.env.DATABASE_NAME 
 
